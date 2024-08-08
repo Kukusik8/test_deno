5f6cc667ca43d4851d3338f23030b7e59be5939c
Ruslan Sakhautdinov <sakhrus14061999@gmail.com>
2024-08-08 14:33:35 +0300

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+
+/* -------------------------------------------------------------------------
+ * Shim.Timestamp
+ * ------------------------------------------------------------------------- */
+
+
+const Timestamp = function (precision = 6) {
+
+    if (!Number.isInteger(precision)) precision = 6;
+    const p = precision;
+
+    const start = typeof performance.timeOrigin !== 'undefined' ? performance.timeOrigin : Date.now();
+    const now = performance.now();
+
+    const iso = new Date(start + now).toISOString();
+
+    const microseconds = (((start + now) % 1000) / 1000).toFixed(p).slice(2);
+
+    return iso.substring(0, iso.indexOf('.') + 1) + microseconds + 'Z';
+};
+
+
+console.log(Timestamp(6));
+console.log(Timestamp(4));
+console.log(Timestamp(3));
